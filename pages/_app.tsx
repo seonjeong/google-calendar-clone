ec37f087612311d324f715f9c6c79d555681a213
seonjeong <theblessedcity@hanmail.net>
2022-08-21 23:25:11 +0900

    [chore] set redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
 
 import type { AppProps } from 'next/app';
+import { wrapper } from '../store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
